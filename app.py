d580e76d9ab251e578c7e4498345cf2ee4dd4a3c
dionguecoumba-lab <diongue.coumba@uam.edu.sn>
2026-05-12 19:58:37 +0000

    change

---
+++ app.py
@@ -0,0 +1,90 @@
+
+import streamlit as st
+
+st.title("ANTA COUMBA DIONGUE")
+st.header("Technicienne superieure en agroalimentaires")
+st.header("_____________________________________")
+st.subheader("Technicienne en sciences alimentaires,je maîtrise les techniques de laboratoire, l’analyse microbiologique et les outils informatiques.Rigoureuse et motivée, je souhaite mettre mes compétences au service de l’innovation agroalimentaire et de la sécurité des produits ")
+
+with st.sidebar:
+    st.image("WhatsApp Image 2026-05-10 at 13.40.53.jpeg")
+    st.write("CONTACT")
+    st.write("📧diongueantacoumba888@gmail.com")
+    st.write("📍Parcelles assainies unité 18")
+    st.write("COMPETENCES")
+    st.write("LOGICIELS MAÎTRISÉS")
+    st.write(" Pack Office ")       
+    st.write("Python : bases")
+    st.write("R (statistiques)")
+   
+st.subheader("FORMATIONS")
+st.expander("License en sciences agronomiques et alimentaires")
+st.expander("2023- 2024: Baccalauréat Scientifique Collège moderne troisième millénaire- Mention BIEN")
+
+st.subheader("EXPERIENCE")
+st.expander("PROJETS ACADÉMIQUES")
+st.write("      * Microbiologie et biologie cellulaires")
+st.write("      * Analyse chimique et thermochimique")
+st.write("      * Suivi de cultures agricoles")
+st.expander("EXPERIENCES PROFESSIONNELLES")
+st.write(" Assistante commerciale en optique: Al Mourtada optique") 
+st.write("     * Accueil et conseil client (choix des montures)")
+st.write("     * Lecture et interprétation d’ordonnances")
+st.write("     * Participation au processus de vente")
+st.write("     * Suivi des ventes")
+st.expander("EXPERIENCES ASSOCIATIVES")
+st.write(" SADAQA ASSOCIATION")
+st.write("    * Responsables des membres")
+st.write("    * Organisation d'actions solidaires")
+st.write("    * Developpement de parstenariats associatifs")
+
+
+st.markdown("""
+   <style>
+       .stApp {
+           background-color: #D3D3D3;  /* gris */
+       }
+       
+       /* Sidebar en gris foncé */
+       [data-testid="stSidebar"] {
+           background-color: #A9A9A9;
+       }
+   </style>
+""", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    [data-testid="stSidebar"] {
+        background-color: #FFB6C1;
+    }
+    </style>
+""", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    [data-testid="stSidebar"] {
+        background-color: #1B2A4A;
+    }
+    </style>
+""", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    [data-testid="stSidebar"] {
+        background-color: #1B2A4A;
+    }
+    [data-testid="stSidebar"] * {
+        color: white !important;
+    }
+    .stApp {
+        background-color: #B8C4E8;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+         
+
+
+
+
+             
+            
+             
+
